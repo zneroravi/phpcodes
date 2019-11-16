534e961887da5270ba0ae4cc6320832cdd4dbaa1
Ravi goswami <zneroravi@gmail.com>
2019-11-16 15:29:35 +0530

    update 4

--- todoList/markComp.php
+++ todoList/markComp.php
@@ -7,4 +7,4 @@ $status = $_GET['status'];
 
 $_SESSION['todo'][$id]['status'] = $status;
 
-echo "Task status changed";
+echo "Task status changed for $id to $status";
--- todoList/showlist.php
+++ todoList/showlist.php
@@ -7,10 +7,35 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script type="text/javascript">
-	$('.box').change( function (event) {
-		// var id = $(this).attr('name');
-		alert(id);
-	})
+	$( document ).ready(function() {
+		$('.box').on('change', function (event) {
+			var id = $(this).attr('id');
+			var status = 0;
+
+			if($(this).prop("checked") == true){
+                status = 1;
+            }
+            else if($(this).prop("checked") == false){
+                status = 0;
+            }
+			$.ajax({
+			    url: "markComp.php",
+			    type: "get", //send it through get method
+			    data: { 
+			        id: id, 
+			        status: status
+			    },
+			    success: function(response) {
+			        alert('status updated');
+			        console.log(response);
+			    },
+			    error: function(xhr) {
+			        alert('some error occured');
+			        console.log(xhr);
+			    }
+			});
+		});
+	});
 </script>
 
 </body>
@@ -22,7 +47,7 @@ session_start();
 $todo = $_SESSION['todo'];
 
 foreach ($todo as $key => $value) {
-	echo "<input type='checkbox' name'$key' class='box' ";
+	echo "<input type='checkbox' id='$key' class='box' ";
 	echo ($value['status'] == 1)?" checked":"";
 	echo " >";
 	echo $value['task'];
