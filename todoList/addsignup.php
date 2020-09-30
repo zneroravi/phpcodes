0ff7baf584c2840acf93795b22f4f89345d19d77
ajankyaknight <72164421+ajankyaknight@users.noreply.github.com>
2020-10-01 01:36:37 +0530

    adding the signup section in todo

---
+++ todoList/addsignup.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>signup</title>
+</head>
+<body>
+<form action="connect.php" method="POST">
+	Name: <input type="text" name="firstname" placeholder="Name"><br>
+
+	Mobile Number: <input type="text" name="mobile" placeholder="Mobile no"><br>
+
+	EmailID: <input type="text" name="email" placeholder="email"><br>
+
+	Password: <input type="password" name="password" placeholder="Password"><br>
+
+	Age: <input type="text" name="age" placeholder="Age"><br>
+	
+	<input type="submit" name="submit">
+
+</form>
+</body>
+</html>
